4602ff36edf440d34e1b0817e2bd49dd3488244f
chaconv <alexroolz@gmail.com>
2022-01-02 18:07:46 +0100

    hero typewritter

--- homepage/src/app/shared/components/hero/hero/hero.component.ts
+++ homepage/src/app/shared/components/hero/hero/hero.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Typewritter } from './hero';
+
+
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    new Typewritter({
+      text: "Alejandro Chacón, Frontend Developer, Desarrollador web 3.0", // palabras usadas para el typewritter
+      time: 90 // velocidad de escritura
+    });
   }
 
 }
